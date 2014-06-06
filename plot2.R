6ec5085f07c68b0a6f2110d7b2cfa4c6af52d7ea
ilkarslan <ilkarslan@gmail.com>
2014-06-07 00:25:20 +0300

    Plot2
    Plot2.R and Plot2.png

---
+++ plot2.R
@@ -0,0 +1,22 @@
+plot2 <- function(){
+      
+      # save household_power_consumption.txt to the working directory
+      
+      url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+      download.file(url, destfile ="power.zip")
+      powdata <- read.table(unz("power.zip","household_power_consumption.txt"),sep=";",header=TRUE, na.strings= "?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+      
+      #get observations of February 1st and 2nd
+      powdata <- powdata[(powdata$Date == "1/2/2007") | (powdata$Date == "2/2/2007"), ]
+      
+      # transform Date using as.Date() function
+      
+      powdata$DateTime <- strptime(paste(powdata$Date, powdata$Time), "%d/%m/%Y %H:%M:%S")
+      
+      
+      #plot 
+      
+      png(filename = "plot2.png", width = 480, height = 480, units = "px")
+      plot(powdata$DateTime, powdata$Global_active_power, type = "l", xlab = "", ylab= "Global Active Power (kilowatts)")
+      dev.off()
+}
