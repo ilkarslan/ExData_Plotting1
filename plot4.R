79adb1447e95d88c8b78bc6b873e27f51ebb0219
ilkarslan <ilkarslan@gmail.com>
2014-06-07 00:43:57 +0300

    plot 4
    plot4.R and plot4.png

---
+++ plot4.R
@@ -0,0 +1,35 @@
+plot4 <- function(){
+      
+      # save household_power_consumption.txt to the working directory
+      
+      url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+      download.file(url, destfile ="power.zip")
+      powdata <- read.table(unz("power.zip","household_power_consumption.txt"),sep=";",header=TRUE, na.strings= "?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+      
+      #get observations of February 1st and 2nd
+      powdata <- powdata[(powdata$Date == "1/2/2007") | (powdata$Date == "2/2/2007"), ]
+      
+      # transform Date using as.Date() function
+      
+      powdata$DateTime <- strptime(paste(powdata$Date, powdata$Time), "%d/%m/%Y %H:%M:%S")
+      
+      
+      #plot 
+      png(filename = "plot4.png", width = 480, height = 480, units = "px")
+      par (mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
+      with(powdata, {
+            plot(DateTime, Global_active_power, xlab="", ylab="Global Active Power", type="l")
+            plot(DateTime, Voltage, xlab="datetime", ylab="Voltage", type="l")
+            
+            cols = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+            plot(DateTime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+            lines(DateTime, Sub_metering_2, type="l", col="red")
+            lines(DateTime, Sub_metering_3, type="l", col="blue")
+            legend("topright", lty=1, lwd=1, col=c("black","blue","red"), legend=cols, bty="n")
+            
+            plot(DateTime, Global_reactive_power, xlab="datetime", ylab="Global_reactive_power", type="l")        
+      })    
+      dev.off()
+      
+      
+}
